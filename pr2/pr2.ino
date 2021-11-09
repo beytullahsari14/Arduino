77888e5f43b6aa3ff2e0599b989bb1e1afd55d89
beytullahsari14 <beytullahsari14@yandex.com>
2021-11-09 16:20:58 +0300

    Initial commit

---
+++ pr2/pr2.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600); 
+
+}
+int sayac=0;
+void loop() {
+  // put your main code here, to run repeatedly:
+Serial.println("Merhaba Arduino");
+delay(1000); 
+sayac=sayac+1;
+sayac=sayac=;
+Serial.print (sayac);
+Serial.println("Merhaba Arduino");
+}
